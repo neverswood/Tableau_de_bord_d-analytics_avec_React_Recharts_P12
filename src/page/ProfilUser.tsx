c1263472c01c8c7b121538ebcd0b4a782ae50f25
neverswood <nevers_wood@hotmail.fr>
2022-08-08 13:53:13 +0200

    Create the panel of activities

--- src/page/ProfilUser.tsx
+++ src/page/ProfilUser.tsx
@@ -1,10 +1,12 @@
 import '../styles/ProfilUser.scss';
 import { Header } from '../components/Header';
+import { PanelActivities } from '../components/PanelActivities';
 
 export function ProfilUser() {
   return (
     <div className="App">
       <Header />
+      <PanelActivities />
     </div>
   );
 }
