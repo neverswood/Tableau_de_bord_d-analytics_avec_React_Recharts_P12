f19c679b7c489fa794842e739163e5152d2694c1
neverswood <nevers_wood@hotmail.fr>
2022-08-18 23:53:59 +0200

    Add data with useEffect for pass the data in the dashboardHeader and panelResults components

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from 'react';
+import { getUser, User } from '../services/User';
 import { DashboardHeader } from './DashboardHeader';
 import { PanelResults } from './PanelResults';
 
 export function Dashboard() {
+  const [userData, setUserData] = useState<User | null>(null);
+
+  useEffect(() => {
+    getUser().then(({ data }) => setUserData(data));
+  }, []);
+  console.log('houp', userData);
+
   return (
     <div className="dashboard">
-      <DashboardHeader />
-      <PanelResults />
+      <DashboardHeader userFirstName={userData?.userInfos.firstName} />
+      <PanelResults keyData={userData?.keyData} />
     </div>
   );
 }
