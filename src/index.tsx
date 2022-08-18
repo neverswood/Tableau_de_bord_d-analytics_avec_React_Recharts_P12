47080028bdcec0b0c6456487f0d3cbf2667053c3
neverswood <nevers_wood@hotmail.fr>
2022-08-18 23:34:23 +0200

    Add the header component in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import { ProfilUser } from './page/ProfilUser';
+import { Header } from './components/Header';
+import { PanelActivities } from './components/PanelActivities';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Header />
     <ProfilUser />
   </React.StrictMode>
 );
