c8c960c44ee33698a345ad4ce44300cbfde1237a
neverswood <nevers_wood@hotmail.fr>
2022-08-18 23:55:11 +0200

    Add the name of the user in a span with the data

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,10 +1,14 @@
 import '../styles/DashboardHeader.scss';
 
-export function DashboardHeader() {
+export function DashboardHeader({
+  userFirstName,
+}: {
+  userFirstName: string | undefined;
+}) {
   return (
     <div className="dashboard__header">
       <p>
-        Bonjour <span className="name">Thomas</span>
+        Bonjour <span className="name">{userFirstName}</span>
       </p>
       <span className="text">
         Félicitation ! Vous avez explosé vos objectifs hier 👏
