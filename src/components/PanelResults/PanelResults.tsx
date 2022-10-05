7f0d23dd21536805d892d0bbcc73bbc2c6d9dfb0
neverswood <nevers_wood@hotmail.fr>
2022-10-05 17:12:31 +0200

    Change the PanelResults function

--- src/components/PanelResults/PanelResults.tsx
+++ src/components/PanelResults/PanelResults.tsx
@@ -3,48 +3,37 @@ import carbsIcon from '../../assets/carbs-icon.jpg';
 import fatIcon from '../../assets/fat-icon.jpg';
 import proteinIcon from '../../assets/protein-icon.jpg';
 import UserDataModel from '../../services/models/UserDataModel';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import './PanelResults.scss';
 import { ResultItem } from './ResultItem';
 
 export function PanelResults({ userData }: { userData: UserDataModel }) {
   return (
-    <div className="resultItemList">
+    <div className="result__item__list">
       <ResultItem
         icon={caloriesIcon}
-        quantity={userData.keyData.calorieCount}
+        quantity={userData.calorieCount}
         unity="KCal"
         quantityName="Calories"
       />
       <ResultItem
         icon={proteinIcon}
-        quantity={userData.keyData.proteinCount}
+        quantity={userData.proteinCount}
         unity="g"
         quantityName="Proteines"
       />
       <ResultItem
         icon={carbsIcon}
-        quantity={userData.keyData.carbohydrateCount}
+        quantity={userData.carbohydrateCount}
         unity="g"
         quantityName="Glucides"
       />
       <ResultItem
         icon={fatIcon}
-        quantity={userData.keyData.lipidCount}
+        quantity={userData.lipidCount}
         unity="g"
         quantityName="Lipides"
       />
     </div>
   );
 }
-
-PanelResults.protoTypes = {
-  data: propTypes.arrayOf(
-    propTypes.shape({
-      icon: propTypes.string,
-      quantity: propTypes.number,
-      unity: propTypes.string,
-      quantityName: propTypes.string,
-    })
-  ),
-};
