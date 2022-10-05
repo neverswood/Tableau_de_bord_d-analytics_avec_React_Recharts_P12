d75d86a284abf17dd7e0fedf1f0cfe50655f8efd
neverswood <nevers_wood@hotmail.fr>
2022-10-05 14:44:21 +0200

    Change the Dashboard function

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { getUser } from '../../services/User';
-
+import PropTypes from 'prop-types';
 import './Dashboard.scss';
-import { BarChartDailyActivity } from '../recharts/BarChartDailyActivity/BarChartDailyActivity';
+import { BarChartDailyActivities } from '../recharts/BarChartDailyActivity/BarChartDailyActivity';
 import { PieChartObjective } from '../recharts/PieChartObjective/PieChartObjective';
 import { RadarChartPerformances } from '../recharts/RadarChartPerformances/RadarChartPerformances';
 import { LineChartSessionDuration } from '../recharts/LineChartSessionDuration/LineChartSessionDuration';
@@ -23,14 +23,14 @@ export function Dashboard({ userId }: { userId: number }): JSX.Element | null {
 
   return (
     <div className="dashboard">
-      <DashboardHeader userFirstName={userData.userInfos.firstName} />
+      <DashboardHeader userFirstName={userData.firstName} />
       <div className="results">
         <div className="charts">
-          <BarChartDailyActivity userId={userId} />
+          <BarChartDailyActivities userId={userId} />
           <div className="charts-under">
             <LineChartSessionDuration userId={userId} />
             <RadarChartPerformances userId={userId} />
-            <PieChartObjective userId={userId} />
+            <PieChartObjective todayScore={userData.todayScore} />
           </div>
         </div>
         <PanelResults userData={userData} />
